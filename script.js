a44e628ad3216dd70e0aa44bfe57c2bebe206b02
Yotagy-code <zhanbolot46@gmail.com>
2025-10-22 19:32:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -131,16 +131,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navLinks = document.querySelector('.nav-links');
 
-  if (menuToggle && navLinks) {
-    menuToggle.addEventListener('click', () => {
-      navLinks.classList.toggle('active');
-    });
-  }
-});
 
 
 
