83a037b1138d1166b3a5c0c0ab2d6b5f53605878
Yotagy-code <zhanbolot46@gmail.com>
2025-10-22 18:48:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -131,10 +131,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-document.querySelectorAll('.nav-links li a').forEach(link => {
-  link.addEventListener('click', () => {
-    if (navLinks.classList.contains('active')) {
-      navLinks.classList.remove('active');
-    }
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
   });
-});
+}
+
+
